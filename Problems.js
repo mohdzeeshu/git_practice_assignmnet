2c0b3743cb8b3aefcf64bb66252411abdc16ab81
Surajcrssiet <115820964+Surajcrssiet@users.noreply.github.com>
2022-10-21 14:27:51 +0530

    Update Problems.js

--- Problems.js
+++ Problems.js
@@ -2,8 +2,29 @@
 
 let arr = [1,2,3,4,5,7,13,15,19,23,21]
 let N =11
+let count =0
+for (let i= 1;i<=num;i++){
+    if (num%i==0){
+        count++
+    }
+}
+if ( count==2){
+    console.log(num,"is a prime number")
+} else{
+    console.log(num , "is not prime number")
+}
 
 
 //Palindrome
 let str = "madam"
 let N = 5
+let N = 5
+let bag ="";
+for(let i= str.length-1; i>=0 ; i--){
+    bag = bag + str[i]
+
+} if (bag == str){
+    console.log("Yes")
+}else{
+    console.log("No")
+}
