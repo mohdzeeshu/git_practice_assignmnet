9a12a8236ef09f876683cd53248bdedd324db108
Surajcrssiet <115820964+Surajcrssiet@users.noreply.github.com>
2022-10-21 15:32:22 +0530

    Update Problems.js
    Sir updated  now please check

--- Problems.js
+++ Problems.js
@@ -1,7 +1,7 @@
 //prime Number
 
 let arr = [1,2,3,4,5,7,13,15,19,23,21]
-let N =11
+let N =13
 let count =0
 for (let i= 1;i<=num;i++){
     if (num%i==0){
@@ -18,7 +18,7 @@ if ( count==2){
 //Palindrome
 let str = "madam"
 let N = 5
-let N = 5
+
 let bag ="";
 for(let i= str.length-1; i>=0 ; i--){
     bag = bag + str[i]
